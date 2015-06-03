ccac9dc0b9ff8ebc239e44cc1336352e4fa02fca
Ilya Kreymer <ikreymer@gmail.com>
2015-06-03 12:39:07 -0700

    browser: reinit driver if it closed chrome (hopefully avoidable with timeout setting)

--- web/browser.py
+++ web/browser.py
@@ -3,6 +3,7 @@ from selenium.webdriver import Chrome, Remote
 from selenium.webdriver.chrome.options import Options
 
 import json
+import logging
 
 
 # ============================================================================
@@ -16,24 +17,34 @@ class ChromeBrowser(object):
 
         self.caps = caps
         self.readlog = readlog
+        self.host_name = host_name
+
+        self._init_driver()
+
+    def _init_driver(self):
         self.driver = None
 
-        if host_name:
+        if self.host_name:
             try:
-                self.driver = Remote(command_executor='http://{0}:4444/wd/hub'.format(host_name),
-                                     desired_capabilities=caps)
+                self.driver = Remote(command_executor='http://{0}:4444/wd/hub'.format(self.host_name),
+                                     desired_capabilities=self.caps)
             except:
                 pass
 
         if not self.driver:
-            self.driver = Chrome(chrome_options=Options(), desired_capabilities=caps)
+            self.driver = Chrome(chrome_options=Options(), desired_capabilities=self.caps)
 
     def close(self):
         if self.driver:
             self.driver.quit()
 
     def visit(self, url):
-        self.driver.start_session(self.caps)
+        try:
+            self.driver.start_session(self.caps)
+        except:
+            logging.debug('Reiniting Driver, probably closed due to timeout')
+            self._init_driver()
+
         self.driver.get(url)
 
         results = {}
